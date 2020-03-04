4aa7509ca7b9edb0863ebe0dcbf91aa8e7658d2b
Elias Ribeiro <profetaelias@msn.com>
2020-03-03 21:12:02 -0300

    implementando upload de arquivos e integrando com a api rest node

--- src/App.js
+++ src/App.js
@@ -1,13 +1,72 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import GlobalStyle from './styles/global'
 import { Container, Content } from './styles'
 import Upload from './components/Upload/'
+import FileList from './components/FileList'
+import { uniqueId } from 'lodash';
+import filesize from 'filesize';
+import api from './services/api';
 
 class App extends Component {
+
+  state = {
+    uploadedFiles: []
+  };
+
+  handleUpload = (files) => {
+    const uploadedFiles = files.map(file => ({
+      file,
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+      preview: URL.createObjectURL(file),
+      progress: 0, 
+      uploaded: false,
+      error: false,
+      url: null,
+    }));
+
+    this.setState({
+      uploadedFiles: this.state.uploadedFiles.concat(uploadedFiles)
+    });
+
+    uploadedFiles.forEach(this.processUpload);
+  };
+
+  uploadFile = (id, data) => {
+    this.setState({uploadedFiles: this.state.uploadedFiles.map(uploadedFile => {
+      return id === uploadedFile.id ? {...uploadedFile, ...data} : uploadedFile;
+    })});
+  }
+
+  processUpload = (uploadedFile) => {
+    const data = new FormData();
+    data.append('file', uploadedFile.file, uploadedFile.name);
+    
+    api.post('/posts', data, {
+      onUploadProgress: e => {
+        const progress = parseInt(Math.round((e.loaded * 100) / e.total));
+        this.uploadFile(uploadedFile.id, {
+          progress: progress,
+          uploaded: progress === 100 ? true : false
+        })
+      }
+    }).catch(error => {
+      
+    });
+  };
+
   render() {
+    const { uploadedFiles } = this.state;
+    
     return <Container>
               <Content>
-                <Upload />
+                <Upload onUpload={this.handleUpload}/>
+
+                { !! uploadedFiles.length && (
+                  <FileList files={uploadedFiles}/>
+                )}
+                
               </Content>
               <GlobalStyle />
            </Container>
--- src/components/FileList/index.js
+++ src/components/FileList/index.js
@@ -1,39 +1,64 @@
 import React from 'react';
-import { Container, FileInfo } from './styles';
-import CircularProgressbar from 'react-circular-progressbar';
+import { Container, FileInfo, Preview } from './styles';
+import {CircularProgressbar} from 'react-circular-progressbar';
 import {MdCheckCircle, MdError, MdLink} from 'react-icons/md';
 
-
-const FileList = () => 
+const FileList = ({files}) => 
     <Container>
-        <li>
-            <FileInfo>
-                <Preview src="https://www-sisgf/SisGF/temas/dataprev2014/logo_dataprev.png" />
-                <div>
-                    <strong>profile.png</strong>
-                    <span>64kb<button onClick={()=>{}}>Excluir</button></span>
-                </div>
-            </FileInfo>
-            <div>
-                <CircularProgressbar styles={{
-                        root: { width:24 },
-                        path: { stroke: '#7159c1'}
-                    }}
-                    strokeWidth={10}
-                    percentege={60}
-                />
-                    <a 
-                        href="https://www-sisgf/SisGF/temas/dataprev2014/logo_dataprev.png"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                    
-                    <MdLink 
+        { 
+            files.map(uploadedFile => (
+                <li key={uploadedFile.id}>
+                    <FileInfo>
+                        <Preview src={uploadedFile.preview} />
+                        <div>
+                            <strong>{uploadedFile.name}</strong>
+                            {uploadedFile.uploaded && (
+                                <span>{uploadedFile.readableSize}<button onClick={()=>{}}>Excluir</button></span>
+                            )}
+                        </div>
+                    </FileInfo>
+                    <div>
+                        {!uploadedFile.error && (
+                            <CircularProgressbar progress={uploadedFile.progress}
+                                styles={{
+                                    text: {
+                                        fill: uploadedFile.progress >= 30 ? 'green' : 'red'
+                                    },
+                                    root: { width: 44 },
+                                    
+                                    path: {
+                                        stroke: '#7159c1',
+                                        strokeLinecap: 'round',
+                                        transition: 'stroke-dashoffset 0.5s ease 0s',
+                                    },
+                                    trail: {
+                                        stroke: '#C0C0C0',
+                                        strokeLinecap: 'round',
+                                    }
+                                }}
+                                initialAnimation={true}
+                                strokeWidth={10}
+                                value={uploadedFile.progress}
+                                text={`${uploadedFile.progress}%`}
+                            />
+                        )}
 
-                />
+                        {uploadedFile.url && (
+                            <a href={uploadedFile.url}
+                               target="_blank"
+                               rel="noopener noreferrer">
+                        
+                               <MdLink style={{ marginRight: 8 }} size={24} color="#222"/>
+                            </a>
+                        )}
 
-            </div>
-        </li>
+                          {!!uploadedFile.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}  
+                          {uploadedFile.error && <MdError size={24} color="#e57878" />}
+                        
+                    </div>
+                </li>
+            ))
+        }
     </Container>
 
 export default FileList;
